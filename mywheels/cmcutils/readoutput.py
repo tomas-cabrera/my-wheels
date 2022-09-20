fb152ae2e0a8b5d87aa94ed833c5c377d34cdda0
tomas-cabrera <tcabrera@andrew.cmu.edu>
2022-09-20 13:34:37 -0400

    Default cmc data reading

--- mywheels/cmcutils/readoutput.py
+++ mywheels/cmcutils/readoutput.py
@@ -2,35 +2,42 @@
 
 ###############################################################################
 
+import inspect
 import numpy as np
 import pandas as pd
 
 ###############################################################################
 
+
 class _output_file:
     """! The parent class for the individual files. """
-    def __init__(self, fname, header_line_no=0, colon_aliases=[]):
+
+    def __init__(
+        self, fname, header_line_no=0, colon_aliases=[], pd_kwargs={},
+    ):
         """! Read the data.
 
         @param  fname           The path to the output file.
         @param  header_line_no  The line to read the columns from.
         @param  colon_aliases   List-like of character to replace with colons (e.g. ["."] for dyn.dat files)
+        @param  pd_kwargs       Dictionary of keyword arguments to pass to pd.read_csv when loading data.
+                                Merges the specified dict with the default to preserve defaults.
 
         """
 
         # Get the column names
-        columns = self._get_column_names(fname, header_line_no, colon_aliases)
-        print(columns)
+        names = self._get_column_names(fname, header_line_no, colon_aliases)
 
-        # TODO: Add functionality to select columns
+        # Merge specified pd_kwargs dist with default
+        pd_kwarg_defaults = {
+            "names": names,
+            "delimiter": " ",
+            "skiprows": header_line_no + 1,
+        }
+        pd_kwargs = {**pd_kwarg_defaults, **pd_kwargs}
 
-        # Load data
-        self.df = pd.read_csv(
-            fname,
-            names=columns,
-            delimiter=" ",
-            skiprows=header_line_no+1,
-        )
+        # Load data, passing usecols to select particular columns
+        self.df = pd.read_csv(fname, **pd_kwargs,)
 
     def _get_column_names(self, fname, header_line_no, colon_aliases=[]):
         """! Parses the specified line in the file to get the header names.
