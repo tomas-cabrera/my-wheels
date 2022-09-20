80340259d9e96eb47a7a7802a9fa2adf1a78ca55
tomas-cabrera <tcabrera@andrew.cmu.edu>
2022-09-20 16:08:07 -0400

    Initial unit conversions, including implementation for dyn.dat

--- mywheels/cmcutils/readoutput.py
+++ mywheels/cmcutils/readoutput.py
@@ -5,6 +5,8 @@
 import numpy as np
 import pandas as pd
 
+import mywheels.cmcutils.cmctoolkit.cmctoolkit as cmctoolkit
+
 ###############################################################################
 
 
@@ -24,6 +26,9 @@ class _dat_file:
 
         """
 
+        # Save fname
+        self.fname = fname
+
         # Get the column names
         names = self._get_column_names(fname, header_line_no, colon_aliases)
 
@@ -38,6 +43,9 @@ class _dat_file:
 
         # Load data, passing usecols to select particular columns
         self.df = pd.read_csv(fname, **pd_kwargs,)
+
+        # Note that units are not converted from original values
+        self.units_converted = {n: False for n in self.df.columns}
 
     def _get_column_names(self, fname, header_line_no, colon_aliases=[]):
         """! Parses the specified line in the file to get the header names.
@@ -63,9 +71,74 @@ class _dat_file:
 
         return names
 
+    def convert_units(self, names_units, conv_fname=None, missing_ok=False):
+        """! Converts specified column(s) with specified conv.sh file.
+
+        @param  names_units     Dictionary, where the keys are the column names to convert and the values are strings of the conv.sh values to use.
+                                " * " and " / " may be used to multiply and divide units, e.g. a value of "cm / nb_s" will make the conversion from nbody velocity to cm/s.
+        @param  conv_fname      Path to the conv.sh file.
+                                By default, this swaps out the file name for "initial.conv.sh"
+        @param  missing_ok      If True, continues if a name is not found in the df column names.
+
+        """ 
+
+        # Make filename, if not specified
+        if conv_fname == None:
+            conv_fname = "/".join((*self.fname.split("/")[:-1], "initial.conv.sh"))
+
+        # Load unitdict
+        unitdict = self._read_unitdict(conv_fname)
+
+        for n,u in names_units.items():
+            # Check if the specified unit is in the df
+            if n in self.df.columns:
+                # Check if the units have already been converted
+                if not self.units_converted[n]:
+                    # Convert units, and mark as converted
+                    self.df[n] *= self._parse_units_string(u, unitdict)
+                    self.units_converted[n] = True
+            elif not missing_ok:
+                raise Exception("Name '%s' not found in columns" % n)
+
+        return 0
+
+    def _read_unitdict(self, conv_fname):
+        """! Reads unitdict from conv.sh.  This function is here to wrap the opening of the file with the cmctoolkit fucntion. """
+
+        f = open(conv_fname, "r")
+        conv_file = f.read().split("\n")
+        f.close()
+        unitdict = cmctoolkit.make_unitdict(conv_file)
+
+        return unitdict
+        
+
+    def _parse_units_string(self, string, unitdict):
+        factor = 1.
+        op = "*"
+        for u in string.split():
+            if u == "*":
+                op = "*"
+            elif u == "/":
+                op = "/"
+            elif op == "*":
+                factor *= unitdict[u]
+                op = None
+            elif op == "/":
+                factor /= unitdict[u]
+                op = None
+            else:
+                raise Exception("Unrecognized sequence in string")
+
+        return factor
+
 class dyn_dat(_dat_file):
     """! Reads the dyn.dat data. """
 
     def __init__(self, fname, **kwargs):
         """! Reads the data, using some defaults for the dyn_dat files. """
         super().__init__(fname, header_line_no=1, colon_aliases=["."], **kwargs)
+
+    def convert_tunits(self, **kwargs):
+        """! By default converts the t and Dt columns into Myr. """
+        self.convert_units({"t": "myr", "Dt": "myr"}, **kwargs)
